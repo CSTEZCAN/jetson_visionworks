e2c5397e043cb58da5b23b3f178a2f45fab852a7
CSTEZCAN <33690601+CSTEZCAN@users.noreply.github.com>
2019-06-30 17:52:44 +0300

    Update install_visionworks.sh

--- install_visionworks.sh
+++ install_visionworks.sh
@@ -1,9 +1,9 @@
 cd /usr/share/visionworks/sources
-sh install-samples.sh /media/nvidia/fit/nano
+sh install-samples.sh /home/nvidia
 cd /usr/share/visionworks-sfm/sources
-sh install-samples.sh /media/nvidia/fit/nano
+sh install-samples.sh /home/nvidia
 cd /usr/share/visionworks-tracking/sources
-sh install-samples.sh /media/nvidia/fit/nano
+sh install-samples.sh /home/nvidia
 cd /media/nvidia/fit/nano/VisionWorks-1.6-Samples
 make -j3
 cd /media/nvidia/fit/nano/VisionWorks-SFM-0.90-Samples
